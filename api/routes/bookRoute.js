15a63133d67c8087740ccd41306c2314ee4b3b5f
Amoz19 <aungmon10co@gmail.com>
2023-01-09 00:30:47 +0630

    remove env file

---
+++ api/routes/bookRoute.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const Book = require('../models/Book')
+
+router.route('/')
+    .get((req, res) => {
+        Book.find()
+            .then(data => res.json(data))
+            .catch(err => console.log(err))
+    })
+
+    .post((req, res) => {
+        const books = new Book(req.body)
+        books.save()
+            .then(res.json(books))
+            .catch(err => console.log(err))
+})
+
+module.exports = router
