0b16d51ef01b24385e91f141e3edf9dbb414864a
Amoz19 <aungmon10co@gmail.com>
2023-01-12 01:02:23 +0630

    react-dom connected

--- api/routes/bookRoute.js
+++ api/routes/bookRoute.js
@@ -1,18 +1,33 @@
-const router = require('express').Router()
+const express = require('express')
+const router = express.Router()
 const Book = require('../models/Book')
 
-router.route('/')
-    .get((req, res) => {
+router.get('/',(req, res) => {
         Book.find()
             .then(data => res.json(data))
             .catch(err => console.log(err))
     })
+    
 
-    .post((req, res) => {
+router.post('/',(req, res) => {
         const books = new Book(req.body)
         books.save()
             .then(res.json(books))
             .catch(err => console.log(err))
+    })
+
+router.get('/:id',(req, res) => {
+    // try{
+    //     const book  = await Book.findById(req.params.id)
+    //     res.json(book)
+    //    }catch(err){
+    //         res.json({message:err})
+    //    }
+
+    Book.findById(req.params.id)
+    .then(data => res.json(data))
+    .catch(err => console.log(err))
+
 })
 
 module.exports = router
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,49 +1,82 @@
 // import {useState } from 'react';
-import HomeUi from './HomeUi';
+// import HomeUi from './HomeUi';
 
-const data = [
-  {
-    _id:"63b91209e4e9cf3c60c0debe",
-    title:"Su Latt",
-    author: "Chan Myae Win",
-    publishedDDate:"5.5.2020",
-    Rating:4,
-    Reviwer: "Aung Mon",
-    Review:"Bla Bla"
-  },
-  {
-    _id:"4500",
-    title:"Hit Refresh",
-    author: "Ye min win",
-    publishedDDate:"19.5.2022",
-    Rating:2,
-    Reviwer: "Amoz",
-    Review:"Ahh hmm"
-  },
+// const data = [
+//   {
+//     _id:"63b91209e4e9cf3c60c0debe",
+//     title:"Su Latt",
+//     author: "Chan Myae Win",
+//     publishedDDate:"5.5.2020",
+//     Rating:4,
+//     Reviwer: "Aung Mon",
+//     Review:"Bla Bla"
+//   },
+//   {
+//     _id:"4500",
+//     title:"Hit Refresh",
+//     author: "Ye min win",
+//     publishedDDate:"19.5.2022",
+//     Rating:2,
+//     Reviwer: "Amoz",
+//     Review:"Ahh hmm"
+//   },
 
-]
+// ]
 
-const Home = () => {
-    // const [bookdata, setBookdata] = useState([data])
+// const Home = () => {
+//     // const [bookdata, setBookdata] = useState([data])
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     const response = await fetch('http://localhost:4000/api/v1/books')
-  //     const json = await response.json()
-  //     setBookdata(json)
-  //   }
+//   // useEffect(() => {
+//   //   async function fetchData() {
+//   //     const response = await fetch('http://localhost:4000/api/v1/books')
+//   //     const json = await response.json()
+//   //     setBookdata(json)
+//   //   }
 
-  //   fetchData()
-  // }, [])
+//   //   fetchData()
+//   // }, [])
 
 
   
-  if (data === []) {
-    return <p>Loading data...</p>;
-  }
-    return ( 
-      <HomeUi data={data} />
-     );
-}
+//   if (data === []) {
+//     return <p>Loading data...</p>;
+//   }
+//     return ( 
+//       <HomeUi data={data} />
+//      );
+// }
  
-export default Home;
+// export default Home;
+
+
+import { useLoaderData, Link } from "react-router-dom";
+
+export default function Home() {
+  const books = useLoaderData()
+  return (
+    <div>
+      {books.map(item => (
+        <Link to={item._id.toString()} key={item._id}>
+           {/* <h2 className='flex h-48 justify-center items-center' >{item.title}</h2> */}
+                <div key={item._id} className="flex flex-col items-center">
+                   <div className="flex flex-col w-1/4 border-b-2 pt-4 border-indigo-300">
+                        <div className=" border-solid border-2 bg-slate-500 text-white">
+                             <h2 className='flex h-48 justify-center items-center' >{item.title}</h2>
+                         </div>
+                        
+                       <p>{item.author}</p>
+                        <pre>{item.publishedDDate}</pre>
+                    </div>
+                    
+              </div>
+          </Link>
+       ))}
+    </div>
+  )
+    
+}
+
+export const booksLoader = async () => {
+  const res = await fetch('http://localhost:4000/api/v1/books')
+  return res.json()
+}
